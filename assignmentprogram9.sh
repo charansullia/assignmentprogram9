3a4e4dff3febc3a94fd7a065d791b326ebf3921d
charasullia <charanrajr713@gmail.com>
2021-07-13 13:48:35 +0530

    Added:assignment9

--- assignmentprogram9.sh
+++ assignmentprogram9.sh
@@ -1,2 +1,10 @@
-# Welcome to assignmentprogram computation
+#!/bin/bash -x
+ispresent=1
+empCheck=$((RANDOM%2))
+if [ 1 -eq $empCheck ]
+then
+echo "emp is present"
+else
+echo "emp is absent"
+fi
 
